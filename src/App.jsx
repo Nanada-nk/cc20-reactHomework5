bc167161e86ee2ea562ea1501e83f9d864a028ba
Nanada-nk <nada.nidchakul@gmail.com>
2025-05-18 22:59:55 +0700

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
 
   return (
-    <div className="w-75 mx-auto my-30 space-y-6">
+    <div className="w-75 sm:w-100 duration-700 mx-auto my-4 space-y-6">
       <h1 className="text-4xl">Select Topping</h1>
       <Topping />
       
